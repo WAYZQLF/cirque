88e3bbf87f163ea7ea943552e461e516632fee07
arigararasa_a <arigararasa_a@D202-022024-09>
2026-02-18 09:33:34 +0100

    Ajoutez des fichiers projet.

---
+++ Consolecirque/Program.cs
@@ -0,0 +1,5 @@
+﻿using cirque;
+Accessoire a1 =new Accessoire("ballon","rouge",0.3,15,5);
+Console.WriteLine(a1.ToString());
+Personnel p1 = new Personnel("clovis", "jongleur");
+Console.WriteLine(p1.ToString());
---
+++ cirque/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace cirque
+{
+    public class Accessoire
+    {
+        private string Nom;
+        private string Couleur;
+        private double Volume;
+        private int Ratelier;
+        private int Camion;
+
+        public Accessoire(string nom, string couleur, double volume, int ratelier, int camion)
+        {
+            this.Nom = nom;
+            this.Couleur = couleur;
+            this.Volume = volume;
+            this.Ratelier = ratelier;
+            this.Camion = camion;
+        }
+        public override string ToString()
+        {
+            return ($"{Nom} {Couleur} Volume {Volume} Camion {Camion}");
+        }
+
+    }
+    public class Personnel
+    {
+        private string Nom_personnel;
+        private string Role_personnel;
+        public Personnel(string nom, string role)
+        {
+            Nom_personnel = nom;
+            Role_personnel = role;
+        }
+        public override string ToString()
+        {
+            return ($"{Nom_personnel}-{Role_personnel}");
+        }
+
+    }
+} 
