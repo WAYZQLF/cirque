ef5462397066ef66f7f3c69a92c893444be10212
WAYZQLF <abiram.arigararasa.sio@gmail.com>
2026-04-01 11:03:07 +0200

    tp5_1

--- frmcirque/FrmMenu.Designer.cs
+++ frmcirque/FrmMenu.Designer.cs
@@ -52,9 +52,20 @@
             btnValiderU = new Button();
             Personnel = new Label();
             Accessoire = new Label();
+            grbNumero = new GroupBox();
+            textNature = new TextBox();
+            textTitre = new TextBox();
+            Accesoires = new Label();
+            Responsable = new Label();
+            Nature = new Label();
+            Titre = new Label();
+            lstAccessoire = new ListBox();
+            cmbResponsable = new ComboBox();
+            btnvaliderN = new Button();
             groupBoxAccessoire.SuspendLayout();
             GroupBoxPersonnel.SuspendLayout();
             groupBoxUtilisation.SuspendLayout();
+            grbNumero.SuspendLayout();
             SuspendLayout();
             // 
             // groupBoxAccessoire
@@ -70,9 +81,9 @@
             groupBoxAccessoire.Controls.Add(Couleur);
             groupBoxAccessoire.Controls.Add(txtNom);
             groupBoxAccessoire.Controls.Add(Nom);
-            groupBoxAccessoire.Location = new Point(57, 34);
+            groupBoxAccessoire.Location = new Point(12, 12);
             groupBoxAccessoire.Name = "groupBoxAccessoire";
-            groupBoxAccessoire.Size = new Size(448, 255);
+            groupBoxAccessoire.Size = new Size(266, 232);
             groupBoxAccessoire.TabIndex = 0;
             groupBoxAccessoire.TabStop = false;
             groupBoxAccessoire.Text = "Accessoire";
@@ -174,7 +185,7 @@
             GroupBoxPersonnel.Controls.Add(Rôle_Personnel);
             GroupBoxPersonnel.Controls.Add(txtNom_Personnel);
             GroupBoxPersonnel.Controls.Add(Nom_Personnel);
-            GroupBoxPersonnel.Location = new Point(511, 36);
+            GroupBoxPersonnel.Location = new Point(284, 38);
             GroupBoxPersonnel.Name = "GroupBoxPersonnel";
             GroupBoxPersonnel.Size = new Size(239, 141);
             GroupBoxPersonnel.TabIndex = 1;
@@ -230,7 +241,7 @@
             groupBoxUtilisation.Controls.Add(btnValiderU);
             groupBoxUtilisation.Controls.Add(Personnel);
             groupBoxUtilisation.Controls.Add(Accessoire);
-            groupBoxUtilisation.Location = new Point(525, 213);
+            groupBoxUtilisation.Location = new Point(529, 45);
             groupBoxUtilisation.Name = "groupBoxUtilisation";
             groupBoxUtilisation.Size = new Size(234, 155);
             groupBoxUtilisation.TabIndex = 2;
@@ -281,11 +292,106 @@
             Accessoire.TabIndex = 0;
             Accessoire.Text = "Accessoire";
             // 
+            // grbNumero
+            // 
+            grbNumero.Controls.Add(btnvaliderN);
+            grbNumero.Controls.Add(cmbResponsable);
+            grbNumero.Controls.Add(lstAccessoire);
+            grbNumero.Controls.Add(textNature);
+            grbNumero.Controls.Add(textTitre);
+            grbNumero.Controls.Add(Accesoires);
+            grbNumero.Controls.Add(Responsable);
+            grbNumero.Controls.Add(Nature);
+            grbNumero.Controls.Add(Titre);
+            grbNumero.Location = new Point(294, 212);
+            grbNumero.Name = "grbNumero";
+            grbNumero.Size = new Size(469, 226);
+            grbNumero.TabIndex = 3;
+            grbNumero.TabStop = false;
+            grbNumero.Text = "Numero";
+            // 
+            // textNature
+            // 
+            textNature.Location = new Point(95, 59);
+            textNature.Name = "textNature";
+            textNature.Size = new Size(100, 23);
+            textNature.TabIndex = 5;
+            // 
+            // textTitre
+            // 
+            textTitre.Location = new Point(95, 21);
+            textTitre.Name = "textTitre";
+            textTitre.Size = new Size(100, 23);
+            textTitre.TabIndex = 6;
+            // 
+            // Accesoires
+            // 
+            Accesoires.AutoSize = true;
+            Accesoires.Location = new Point(12, 151);
+            Accesoires.Name = "Accesoires";
+            Accesoires.Size = new Size(63, 15);
+            Accesoires.TabIndex = 3;
+            Accesoires.Text = "Accessoire";
+            // 
+            // Responsable
+            // 
+            Responsable.AutoSize = true;
+            Responsable.Location = new Point(12, 102);
+            Responsable.Name = "Responsable";
+            Responsable.Size = new Size(73, 15);
+            Responsable.TabIndex = 2;
+            Responsable.Text = "Responsable";
+            // 
+            // Nature
+            // 
+            Nature.AutoSize = true;
+            Nature.Location = new Point(12, 59);
+            Nature.Name = "Nature";
+            Nature.Size = new Size(43, 15);
+            Nature.TabIndex = 1;
+            Nature.Text = "Nature";
+            // 
+            // Titre
+            // 
+            Titre.AutoSize = true;
+            Titre.Location = new Point(12, 29);
+            Titre.Name = "Titre";
+            Titre.Size = new Size(30, 15);
+            Titre.TabIndex = 0;
+            Titre.Text = "Titre";
+            // 
+            // lstAccessoire
+            // 
+            lstAccessoire.FormattingEnabled = true;
+            lstAccessoire.ItemHeight = 15;
+            lstAccessoire.Location = new Point(95, 135);
+            lstAccessoire.Name = "lstAccessoire";
+            lstAccessoire.Size = new Size(97, 64);
+            lstAccessoire.TabIndex = 7;
+            // 
+            // cmbResponsable
+            // 
+            cmbResponsable.FormattingEnabled = true;
+            cmbResponsable.Location = new Point(95, 94);
+            cmbResponsable.Name = "cmbResponsable";
+            cmbResponsable.Size = new Size(121, 23);
+            cmbResponsable.TabIndex = 8;
+            // 
+            // btnvaliderN
+            // 
+            btnvaliderN.Location = new Point(288, 89);
+            btnvaliderN.Name = "btnvaliderN";
+            btnvaliderN.Size = new Size(75, 23);
+            btnvaliderN.TabIndex = 9;
+            btnvaliderN.Text = "valider";
+            btnvaliderN.UseVisualStyleBackColor = true;
+            // 
             // FrmMenu
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(grbNumero);
             Controls.Add(groupBoxUtilisation);
             Controls.Add(GroupBoxPersonnel);
             Controls.Add(groupBoxAccessoire);
@@ -297,6 +403,8 @@
             GroupBoxPersonnel.PerformLayout();
             groupBoxUtilisation.ResumeLayout(false);
             groupBoxUtilisation.PerformLayout();
+            grbNumero.ResumeLayout(false);
+            grbNumero.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -326,5 +434,15 @@
         private Label Accessoire;
         private ComboBox cmbAccessoire;
         private ComboBox cmbPersonnnel;
+        private GroupBox grbNumero;
+        private Label Accesoires;
+        private Label Responsable;
+        private Label Nature;
+        private Label Titre;
+        private TextBox textNature;
+        private TextBox textTitre;
+        private Button btnvaliderN;
+        private ComboBox cmbResponsable;
+        private ListBox lstAccessoire;
     }
 }
--- frmcirque/FrmMenu.cs
+++ frmcirque/FrmMenu.cs
@@ -17,11 +17,17 @@ namespace frmcirque
 
         private void btnValiderA_Click(object sender, EventArgs e)
         {
-            listAccessoire.Add(new Accessoire(txtNom.Text, txtCouleur.Text, Convert.ToDouble(txtVolume.Text), Convert.ToInt32(txtRatelier.Text), Convert.ToInt32(txtCamion.Text)));
+            listAccessoire.Add(new Accessoire(txtNom.Text, txtCouleur.Text,
+                Convert.ToDouble(txtVolume.Text),
+                Convert.ToInt32(txtRatelier.Text),
+                Convert.ToInt32(txtCamion.Text)));
+
             cmbAccessoire.Items.Clear();
+            lstAccessoire.Items.Clear();
             foreach (Accessoire accessoire in listAccessoire)
             {
                 cmbAccessoire.Items.Add(accessoire.getNom());
+                lstAccessoire.Items.Add(accessoire.getNom());
             }
             txtNom.Clear();
             txtCouleur.Clear();
@@ -33,20 +39,22 @@ namespace frmcirque
         private void btnvaliderP_Click(object sender, EventArgs e)
         {
             listPersonnel.Add(new Personnel(txtNom_Personnel.Text, txtRôle_Personnel.Text));
+
             cmbPersonnnel.Items.Clear();
-            foreach(Personnel personnel in listPersonnel)
+            cmbResponsable.Items.Clear();
+            foreach (Personnel personnel in listPersonnel)
             {
                 cmbPersonnnel.Items.Add(personnel.getNomP());
-            }   
+                cmbResponsable.Items.Add(personnel.getNomP());
+            }
             txtNom_Personnel.Clear();
             txtRôle_Personnel.Clear();
-
         }
 
         private void btnValiderU_Click(object sender, EventArgs e)
         {
             Accessoire a = new Accessoire();
-            string accessoire=cmbAccessoire.SelectedItem.ToString();
+            string accessoire = cmbAccessoire.SelectedItem.ToString();
             foreach (Accessoire acces in listAccessoire)
             {
                 if (acces.getNom() == accessoire)
@@ -55,7 +63,7 @@ namespace frmcirque
                 }
             }
             Personnel p = new Personnel();
-            string personnel=cmbPersonnnel.SelectedItem.ToString();
+            string personnel = cmbPersonnnel.SelectedItem.ToString();
             foreach (Personnel perso in listPersonnel)
             {
                 if (perso.getNomP() == personnel)
@@ -68,6 +76,33 @@ namespace frmcirque
             // Désélectionner sans vider la liste
             cmbAccessoire.SelectedIndex = -1;
             cmbPersonnnel.SelectedIndex = -1;
+        }
+        private void btnvaliderN_Click(object sender, EventArgs e)
+        {
+            // a. Trouver le personnel sélectionné
+            Personnel p = new Personnel();
+            string nomPersonnel = cmbResponsable.SelectedItem.ToString();
+            foreach (Personnel perso in listPersonnel)
+            {
+                if (perso.getNomP() == nomPersonnel)
+                {
+                    p = perso;
+                }
+            }
+
+            // b. Enregistrer les sélections multiples d'accessoires
+            Utilisation u = new Utilisation(p, new Accessoire());
+            foreach (var item in lstAccessoire.SelectedItems)
+            {
+                u.UtilisationsAccessoires.Add(item.ToString());
+            }
+            listUtilisation.Add(u);
+
+            // Réinitialiser
+            cmbResponsable.SelectedIndex = -1;
+            lstAccessoire.ClearSelected();
+            textTitre.Clear();
+            textNature.Clear();
         }
     }
 }
